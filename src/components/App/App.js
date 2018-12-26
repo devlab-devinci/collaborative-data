1aae44930dc96e2f99b46e9f456a93cdbbb92588
Florian <florian.kelnerowski@aphp.fr>
2018-12-26 14:21:21 +0100

    sign up / in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
 
 import Navigation from '../Navigation/Navigation';
 import LandingPage from '../Landing/Landing';
@@ -13,21 +16,21 @@ import AdminPage from '../Admin/Admin';
 import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-  <Router>
-    <div>
-      <Navigation />
+    <Router>
+      <div>
+        <Navigation />
 
-      <hr />
+        <hr />
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
+      </div>
+    </Router>
 );
 
 export default App;
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,3 +5,4 @@ export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
 export const ADMIN = '/admin';
+export const ADMIN_DETAILS = '/admin/:id';
